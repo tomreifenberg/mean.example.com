f9970ef51c1d034cc43e25781ba3060c7b526b8b
tom reifenberg <reifenberg.57@gmail.com>
2020-08-25 16:19:04 -0500

    Add gulp processes for the user app

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,18 @@ gulp.task('build-auth-js', function() {
   return merge(authApp);
 });
 
+gulp.task('build-users-js', function() {
+
+    var userApp = gulp.src([
+      'src/js/users.app.js',
+    ])
+    .pipe(concat('users.app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/js'));
+  
+    return merge(userApp);
+  });
+
 //Recompile SCSS/JS on save
 gulp.task('watch', function(){
   gulp.watch('./src/scss/**/*.scss', gulp.series('build-css'));
@@ -58,7 +70,8 @@ gulp.task('default', gulp.series('watch'));
 gulp.task('build-css', gulp.series('build-main-css'));
 
 //Compile all JS tasks
-gulp.task('build-js', gulp.series(
-    'build-main-js',
-    'build-auth-js',
-  ));
+gulp.task('build-js', [
+  'build-main-js',
+  'build-auth-js',
+  'build-users-js'
+]);
