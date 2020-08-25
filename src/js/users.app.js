d044a833d8e7bb0228f35c06e3b6c62320448a66
tom reifenberg <reifenberg.57@gmail.com>
2020-08-25 16:37:44 -0500

    Pull a list of users from the API

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,11 +1,31 @@
 var usersApp = (function() {
 
-    return {
-      load: function(){
-        alert('LOADED');
+    function viewUsers(){
+  
+      let uri = `${window.location.origin}/api/users`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', uri);
+  
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+  
+      xhr.send();
+  
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+        console.log(data);
       }
     }
   
-  })();
-  
-  usersApp.load();
+    return {
+      load: function(){
+        alert('LOADED');
+        viewUsers();
+      }
+    }
+
+})();
+
+usersApp.load();
