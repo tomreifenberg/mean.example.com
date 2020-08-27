c2ff4191d2a5537924f2a16a3ada4b58dd6fbffc
tom reifenberg <reifenberg.57@gmail.com>
2020-08-27 09:12:01 -0500

    Mock-up the navigation

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -61,11 +61,33 @@ var usersApp = (function() {
     }
   
     return {
-      load: function(){
-        alert('LOADED');
-        viewUsers();
-      }
-    }
+        load: function(){
+            let hash = window.location.hash;
+            let hashArray = hash.split('-');
+          
+            switch(hashArray[0]){
+              case '#create':
+                console.log('CREATE');
+                break;
+          
+              case '#view':
+                console.log('VIEW');
+                break;
+          
+              case '#edit':
+                console.log('EDIT');
+                break;
+          
+              case '#delete':
+                console.log('DELETE');
+                break;
+          
+              default:
+                viewUsers();
+                break;
+            }
+          }
+        }
 
 })();
 
