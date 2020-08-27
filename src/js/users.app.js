c0c2b0268f2c39d64c884c3b84c54c83047fa845
tom reifenberg <reifenberg.57@gmail.com>
2020-08-27 09:40:19 -0500

    Add a common method for processing web forms

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -19,7 +19,7 @@ var usersApp = (function() {
         let users = data.users;
         let table = '';
         let rows = '';
-      
+  
         //Loop each user record into it's own HTML table row, each user should
         //have a link a user view
         for (let i=0; i<users.length; i++) {
@@ -31,7 +31,7 @@ var usersApp = (function() {
             <td>${users[i]['email']}</td>
           </tr>`;
         }
-      
+  
         //Create a users panel, add a table to the panel, inject the rows into the
         //table
         table = `<div class="card">
@@ -54,92 +54,125 @@ var usersApp = (function() {
             </table>
           </div>
         </div>`;
-      
+  
         //Append the HTML to the #app
         app.innerHTML = table;
       }
     }
-
+  
     function createUser(){
-        var app = document.getElementById('app');
-      
-        var form =  `
-            <div class="card">
-              <div class="card-header clearfix">
-                <h2 class="h3 float-left">Create a New User</h2>
-                <div class="float-right">
-                  <a href="#" class="btn btn-primary">Cancel</a>
-                </div>
-              </div>
-              <div class="card-body">
-                <form id="registrationForm" class="card-body">
-                  <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
-      
-                  <div class="row">
-                    <div class="form-group col-md-6">
-                      <label for="first_name">First Name</label>
-                      <input type="text" id="first_name" name="first_name" class="form-control" required>
-                    </div>
-      
-                    <div class="form-group col-md-6">
-                      <label for="last_name">Last Name</label>
-                      <input type="text" id="last_name" name="last_name" class="form-control" required>
-                    </div>
-                  </div>
-      
-                  <div class="row">
-                    <div class="form-group col-md-6">
-                      <label for="username">Username</label>
-                      <input type="text" id="username" name="username" class="form-control" required>
-                    </div>
-      
-                    <div class="form-group col-md-6">
-                      <label for="email">Email</label>
-                      <input type="email" id="email" name="email" class="form-control" required>
-                    </div>
-                  </div>
-      
-                  <div class="text-right">
-                    <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
-                  </div>
-                </form>
+      var app = document.getElementById('app');
+  
+      var form =  `
+          <div class="card">
+            <div class="card-header clearfix">
+              <h2 class="h3 float-left">Create a New User</h2>
+              <div class="float-right">
+                <a href="#" class="btn btn-primary">Cancel</a>
               </div>
             </div>
-        `;
-      
-        app.innerHTML=form;
-      }
-      
+            <div class="card-body">
+              <form id="createUser" class="card-body">
+                <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
   
-    return {
-        load: function(){
-            let hash = window.location.hash;
-            let hashArray = hash.split('-');
-          
-            switch(hashArray[0]){
-                case '#create':
-                    createUser();
-                    break;
-          
-              case '#view':
-                console.log('VIEW');
-                break;
-          
-              case '#edit':
-                console.log('EDIT');
-                break;
-          
-              case '#delete':
-                console.log('DELETE');
-                break;
-          
-              default:
-                viewUsers();
-                break;
-            }
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="first_name">First Name</label>
+                    <input type="text" id="first_name" name="first_name" class="form-control" required>
+                  </div>
+  
+                  <div class="form-group col-md-6">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" id="last_name" name="last_name" class="form-control" required>
+                  </div>
+                </div>
+  
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" class="form-control" required>
+                  </div>
+  
+                  <div class="form-group col-md-6">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                  </div>
+                </div>
+  
+                <div class="text-right">
+                  <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+                </div>
+              </form>
+            </div>
+          </div>
+      `;
+  
+      app.innerHTML=form;
+    }
+  
+    function postRequest(formId, url){
+      let form = document.getElementById(formId);
+      form.addEventListener('submit', function(e){
+        e.preventDefault();
+  
+        let formData = new FormData(form);
+        let uri = `${window.location.origin}${url}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', uri);
+  
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+  
+        let object = {};
+        formData.forEach(function(value, key){
+          object[key]=value;
+        });
+  
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          if(data.success===true){
+            window.location.hash = `#view-${data.user._id}`
+          }else{
+            document.getElementById('formMsg').style.display='block';
           }
         }
-
-})();
-
-usersApp.load();
+      });
+    }
+  
+    return {
+      load: function(){
+        let hash = window.location.hash;
+        let hashArray = hash.split('-');
+  
+        switch(hashArray[0]){
+          case '#create':
+            createUser();
+            postRequest('createUser', '/api/users');
+            break;
+  
+          case '#view':
+            console.log('VIEW');
+            break;
+  
+          case '#edit':
+            console.log('EDIT');
+            break;
+  
+          case '#delete':
+            console.log('DELETE');
+            break;
+  
+          default:
+            viewUsers();
+            break;
+        }
+      }
+    }
+  
+  })();
+  
+  usersApp.load();
+  
